44412f6597d2facef701480ed5c75f185d2d21c0
DanyloBoleyko <ipz20bd-201@stu.zsea.edu.ua>
2021-05-21 13:47:38 +0300

    Added XML comments
    + comments were translated to english

--- Calculator/CalculatorCore.cpp
+++ Calculator/CalculatorCore.cpp
@@ -1,28 +1,40 @@
-﻿#include<iostream>
-#include<string>
+﻿#include <iostream>
+#include <string>
 #include "Number.h"
 
 using namespace std;
 
-/* Образи функцій */
+/// <summary>
+/// Get postfix notation
+/// </summary>
+/// <param name="input">- your string</param>
+/// <returns>postfix form</returns>
 string toPostfix(string input);
+/// <summary>
+/// Calculate postfix notation
+/// </summary>
+/// <param name="input">- postfix form</param>
+/// <returns>answer</returns>
 double postfixCalculate(string reversed);
 
-/* Головна функція */
+/// <summary>
+/// Main function
+/// </summary>
+/// <returns>0</returns>
 int main() {
 	string string = "", substr = "", postfix;
 	int basis, toBasis, task;
 	double answer;
 	char symbol;
 	do {
-		/* Вибір завдання або вихід з програми */
+		/* Task chooser */
 		cout << "0 - out; " << endl;
 		cout << "1 - simple calculator; " << endl;
 		cout << "2 - numerical systems calculator. " << endl;
 		do {
 			cin >> task;
 		} while (task < 0 || task > 2);
-		/* Звичайний калькулятор */
+		/* First task */
 		if (task == 1) {
 			cout << "Enter your formula: " << endl;
 			string = "";
@@ -41,7 +53,7 @@ int main() {
 			cout << num.get(2) << " (" << 2 << ")" << endl;
 			cout << num.get(8) << " (" << 8 << ")" << endl;
 			cout << num.get(16) << " (" << 16 << ")" << endl;
-		} /* Калькулятор систем числення */
+		} /* Second task */
 		else if (task == 2) {
 			cout << "Enter your number (ex. AF.32 (16)): " << endl;
 			cin >> string;
