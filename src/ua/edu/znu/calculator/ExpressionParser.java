65e54b34b5dc8d66a9bdec7faf098818672bafcc
Volodymyr Novak <vn2308549@gmail.com>
2026-05-31 18:41:11 +0300

    Додано логіку ExpressionParser та PostfixEvaluator

--- src/ua/edu/znu/calculator/ExpressionParser.java
+++ src/ua/edu/znu/calculator/ExpressionParser.java
@@ -1,5 +1,6 @@
 package ua.edu.znu.calculator;
 
+import java.util.Stack;
 
 /**
  * Виконує аналіз введеного рядка та перетворення інфіксного виразу в постфіксну нотацію
@@ -8,12 +9,78 @@ package ua.edu.znu.calculator;
  */
 public class ExpressionParser {
 
-    // Приймає рядок з інфіксним виразом та повертає рядок
-//    public static String InfixToPostfix(String infix) {
-//
-//    }
-// повертає пріоритет оператора (^ 4, * / % 3, + − 2)
-//    private static int getPrecedence(char op) {
-//
-//    }
+    public static String infixToPostfix(String vhidnyjRjadok) {
+        StringBuilder rezultat = new StringBuilder();
+        Stack<Character> stekOperatoriv = new Stack<>();
+
+        if (vhidnyjRjadok.endsWith("=")) {
+            vhidnyjRjadok = vhidnyjRjadok.substring(0, vhidnyjRjadok.length() - 1);
+        }
+        for (int i = 0; i < vhidnyjRjadok.length(); i++) {
+            char symvol = vhidnyjRjadok.charAt(i);
+
+            if (Character.isWhitespace(symvol)) {
+                continue;
+            }
+            // на початку числа якщо символ є цифрою або крапкою
+            if (Character.isDigit(symvol) || symvol == '.') {
+                while (i < vhidnyjRjadok.length() && (Character.isDigit(vhidnyjRjadok.charAt(i)) || vhidnyjRjadok.charAt(i) == '.')) {
+                    rezultat.append(vhidnyjRjadok.charAt(i));
+                    i++;
+                }
+                rezultat.append(" ");
+                i--;
+            }
+            // зустріли відкриваючу дужку
+            else if (symvol == '(') {
+                stekOperatoriv.push(symvol);
+            }
+            //зустріли закриваючу дужку
+            else if (symvol == ')') {
+                while (!stekOperatoriv.isEmpty() && stekOperatoriv.peek() != '(') {
+                    rezultat.append(stekOperatoriv.pop()).append(" ");
+                }
+                if (!stekOperatoriv.isEmpty() && stekOperatoriv.peek() == '(') {
+                    stekOperatoriv.pop();
+                }
+            }
+            //зустріли математичний оператор (+ - * / % ^)
+            else if (isOperator(symvol)) {
+                while (!stekOperatoriv.isEmpty() && isOperator(stekOperatoriv.peek())) {
+                    char staryjZnak = stekOperatoriv.peek();
+                    int priorytetStarogo = getPrecedence(staryjZnak);
+                    int priorytetNovogo = getPrecedence(symvol);
+                    boolean staryjSylnishyj = priorytetStarogo > priorytetNovogo;
+                    boolean odnakoviTaNeStepen = (priorytetStarogo == priorytetNovogo) && symvol != '^';
+
+                    if (staryjSylnishyj || odnakoviTaNeStepen) {
+                        rezultat.append(stekOperatoriv.pop()).append(" ");
+                    } else {
+                        break;
+                    }
+                }
+                stekOperatoriv.push(symvol);
+            }
+        }
+        //Вивантаження залишків знаків зі стеку в результат
+        while (!stekOperatoriv.isEmpty()) {
+            rezultat.append(stekOperatoriv.pop()).append(" ");
+        }
+        return rezultat.toString().trim();
+    }
+    private static int getPrecedence(char operator) {
+        if (operator == '^') {
+            return 4;
+        }
+        if (operator == '*' || operator == '/') {
+            return 3;
+        }
+        if (operator == '+' || operator == '-') {
+            return 2;
+        }
+        return -1;
+    }
+    private static boolean isOperator(char symvol) {
+        return symvol == '+' || symvol == '-' || symvol == '*' || symvol == '/' || symvol == '^';
+    }
 }
--- src/ua/edu/znu/calculator/PostfixEvaluator.java
+++ src/ua/edu/znu/calculator/PostfixEvaluator.java
@@ -1,14 +1,39 @@
 package ua.edu.znu.calculator;
 
+import java.util.Stack;
+
 /**
- * Виконує обчислення постфіксного виразу
+ * Обчислення результату за постфіксним записом
  * @author Novak
  * @version 1.0
  */
 public class PostfixEvaluator {
+    public static double evaluate(String postfix) {
+        Stack<Double> stek = new Stack<>(); // Стек, де будуть зберігатися тільки числа
 
-    // Обчислює значення постфіксного виразу та повертає результат.
-//    public static double evaluate(String postfix){
-//
-//    }
+        // Розбиваємо наш рядок на окремі елементи за допомогою пробілів
+        String[] elementy = postfix.split(" ");
+        for (String x : elementy) {
+            // пропуск порожнього елемента
+            if (x.isEmpty()) {
+                continue;
+            }
+            if (x.equals("+") || x.equals("-") || x.equals("*") || x.equals("/") || x.equals("^")) {
+                double b = stek.pop(); // зі стеку друге число
+                double a = stek.pop(); // зі стеку перше число
+                // Рахуємо результат залежно від знака і відразу кладемо його назад у стек
+                if (x.equals("+")) stek.push(a + b);
+                else if (x.equals("-")) stek.push(a - b);
+                else if (x.equals("*")) stek.push(a * b);
+                else if (x.equals("/")) stek.push(a / b);
+                else if (x.equals("^")) stek.push(Math.pow(a, b));
+            }
+            else {
+                // Перетворюємо текст на звичайне комп'ютерне число та кладемо в стек
+                double chislo = Double.parseDouble(x);
+                stek.push(chislo);
+            }
+        }
+        return stek.pop();
+    }
 }
