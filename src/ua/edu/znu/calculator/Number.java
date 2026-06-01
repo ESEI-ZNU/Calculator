18d676233143d8542c6ce1656cddeabf9f0a1887
MyroniukOleksandr <sunumir29@gmail.com>
2026-06-01 12:41:25 +0300

    виправлен баг що метод convertHexToDecimal не працював з маленькими літерами

--- src/ua/edu/znu/calculator/Number.java
+++ src/ua/edu/znu/calculator/Number.java
@@ -65,33 +65,35 @@ package ua.edu.znu.calculator;
 
     }
 }
- 
 
 
-    public static String convertDecimalToHex(int number){//10->16
+    public static String convertDecimalToHex(int number) {//10->16
         StringBuilder hex = new StringBuilder();
         while (number > 0) {
-            int remainder =(number % 16);
-            if(remainder <10) {
-                hex.insert(0,remainder);
+            int remainder = (number % 16);
+            if (remainder < 10) {
+                hex.insert(0, remainder);
             } else {
-                hex.insert(0, (char)('a' + (remainder - 10)));
+                hex.insert(0, (char) ('a' + (remainder - 10)));
             }
-            number /=16;
+            number /= 16;
         }
         return hex.toString();
     }
-    public static long convertHexToDecimal(String number){//16->10
+
+    public static long convertHexToDecimal(String number) {//16->10
+        number = number.toUpperCase();
         long decimal = 0;
-        for(int i = 0; i <number.length();i++){
+        for (int i = 0; i < number.length(); i++) {
             char c = number.charAt(i);
             int digit = 0;
-            if(c >='0'&& c<= '9'){
-                digit = c-'0';
-            } else if (c >= 'A' && c <= 'F'){
-                digit = 10 + (c -'A');
+            if (c >= '0' && c <= '9') {
+                digit = c - '0';
+            } else if (c >= 'A' && c <= 'F') {
+                digit = 10 + (c - 'A');
             }
-            decimal = decimal * 16 + digit;}
+            decimal = decimal * 16 + digit;
+        }
         return decimal;
     }
 }
